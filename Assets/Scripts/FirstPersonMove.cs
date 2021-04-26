cdff3e0d384288507015f49c7e7fef4be5a036f1
James Kent <kentja18@students.ecu.edu>
2021-04-25 22:20:35 -0400

    Added jump & gravity

--- Assets/Scripts/FirstPersonMove.cs
+++ Assets/Scripts/FirstPersonMove.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class FirstPersonMove : MonoBehaviour
 {
 	public float speed = 10f;
+	public float gravity = 10f;
+	public float jumpSpeed = 2.0f;
 	public bool canMove = true;
 
 	private CharacterController charContr;
+	private Vector3 jumpVect = Vector3.zero;
 
 	// Use this for initialization
 	void Start()
@@ -23,7 +26,20 @@ public class FirstPersonMove : MonoBehaviour
 
 		Vector3 forward = transform.forward * v * speed * Time.deltaTime; // Vector3 for movement
 		Vector3 sideways = transform.right * h * speed * Time.deltaTime;
+		Vector3 grav = new Vector3(0, -gravity, 0); // Gravity
 
-		if (canMove) charContr.Move(forward + sideways); // Moves object (player)
+		if (canMove && charContr.isGrounded) charContr.Move(forward + sideways + grav); // Moves object (player)
+		else
+		{
+			charContr.Move(forward + sideways + (jumpVect * Time.deltaTime));
+			jumpVect.y -= gravity * Time.deltaTime;
+		}
+
+		if (charContr.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+			jumpVect.y = jumpSpeed;
+			charContr.Move(forward + sideways + (jumpVect * Time.deltaTime));
+		}
+		
 	}
 }
