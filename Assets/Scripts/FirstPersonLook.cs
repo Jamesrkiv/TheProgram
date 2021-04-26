2bea396843905251bce36ed4c040f838be368ae1
James Kent <kentja18@students.ecu.edu>
2021-04-25 21:50:54 -0400

    Basic first person controller

---
+++ Assets/Scripts/FirstPersonLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonLook : MonoBehaviour
+{
+    public float lookSpeed = 3;
+    private Vector2 rotation = Vector2.zero;
+
+    public bool isPlaying = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying) Cursor.lockState = CursorLockMode.Locked; // Locks the mouse
+
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+        transform.eulerAngles = new Vector2(0, rotation.y) * lookSpeed;
+        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
+    }
+}
---
+++ Assets/Scripts/FirstPersonMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMove : MonoBehaviour
+{
+	public float speed = 10f;
+	public bool canMove = true;
+
+	private CharacterController charContr;
+
+	// Use this for initialization
+	void Start()
+	{
+		charContr = GetComponent<CharacterController>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float h = Input.GetAxis("Horizontal"); // Gets input axis
+		float v = Input.GetAxis("Vertical");
+
+		Vector3 forward = transform.forward * v * speed * Time.deltaTime; // Vector3 for movement
+		Vector3 sideways = transform.right * h * speed * Time.deltaTime;
+
+		if (canMove) charContr.Move(forward + sideways); // Moves object (player)
+	}
+}
