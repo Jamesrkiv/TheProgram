732ec9c102ed69dae4592c094f4959f8d3189554
Jacob <hatcherj17@students.ecu.edu>
2021-04-30 22:38:36 -0400

    Added Random Weapon Start...
     and Random Gun with Random Ammo when ammo hit 0

---
+++ Assets/Scripts/PlayerWeapons.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerWeapons : MonoBehaviour
+{
+    public GameObject[] weapons;
+    public GameObject[] projectiles;
+    public int ammoCount;
+    public int maxAmmo;
+    public Text ammo;
+
+    private GameObject randomGun;
+
+    private void Awake()
+    {
+        RandomStartGun();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammo.text = ammoCount.ToString() + "/" + maxAmmo.ToString();
+        if (ammoCount <= 0)
+        {
+            Destroy(randomGun);
+            StartCoroutine(RandomGunSelect());
+            ammoCount = 1;
+
+        }
+    }
+
+    void RandomStartGun()
+    {
+        randomGun = Instantiate(weapons[Random.Range(0, weapons.Length)], transform.position, transform.rotation);
+        randomGun.transform.parent = gameObject.transform;
+    }
+
+    IEnumerator RandomGunSelect()
+    {
+            yield return new WaitForSeconds(Random.Range(.5f, 2.5f));
+            randomGun = Instantiate(weapons[Random.Range(0, weapons.Length)], transform.position, transform.rotation);
+            randomGun.transform.parent = gameObject.transform;
+            ammoCount += Random.Range(10, maxAmmo);
+    }
+}
--- Assets/Scripts/projectileGunScript.cs
+++ Assets/Scripts/projectileGunScript.cs
@@ -5,17 +5,14 @@ public class projectileGunScript : MonoBehaviour
 {
     public GameObject projectilePrefab; //Bullet prefab
     public GameObject gunBarrel; //Gun barrel Position
-    public int maxAmmo;
-    public int ammo;
-    public Text ammoCount;
 
     public bool debugLaser = false;
+    private PlayerWeapons gunBelt;
 
     public float speed = 20;
-
-    void Start()
+    private void Start()
     {
-
+        gunBelt = GetComponentInParent<PlayerWeapons>();
     }
 
     // Update is called once per frame
@@ -23,17 +20,15 @@ public class projectileGunScript : MonoBehaviour
     {
         if (debugLaser) Aim(); //Laser for Debuging Aim from gunBarrel Position
 
-        ammoCount.text = ammo.ToString() + "/" + maxAmmo.ToString();
-
-        if (Input.GetKeyDown(KeyCode.Mouse0) && (ammo > 0))
-        { 
-            ammo--;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
             fireWeapon();
         }
     }
 
     void fireWeapon()
     {
+        gunBelt.ammoCount -= 1;
         GameObject bullet = Instantiate(projectilePrefab, gunBarrel.transform.position, gunBarrel.transform.rotation);
         bullet.GetComponent<Rigidbody>().AddForce(transform.forward * speed * Time.deltaTime);
     }
