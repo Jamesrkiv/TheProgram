7fb060d1c5b0e585696f94feb3d440f05f29252b
Jacob <hatcherj17@students.ecu.edu>
2021-05-02 18:34:08 -0400

    Small update to starting Ammo so its a bit more random/organized PlayerScripts and EnemyScripts  a bit

---
+++ Assets/Scripts/EnemyScripts/Target.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target : MonoBehaviour
+{
+
+    public int health = 100;
+    public Slider slider;
+    private void Awake()
+    {
+        slider.maxValue = 100;
+    }
+    void Update()
+    {
+        setHealth(health);
+    }
+    public void setHealth(int health)
+    {
+
+        slider.value = health;
+    }
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/projectileGunScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class projectileGunScript : MonoBehaviour
+{
+    public GameObject projectilePrefab; //Bullet prefab
+    public GameObject gunBarrel; //Gun barrel Position
+
+    public bool debugLaser = false;
+    private PlayerWeapons gunBelt;
+
+    public float speed = 20;
+    private void Start()
+    {
+        gunBelt = GetComponentInParent<PlayerWeapons>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (debugLaser) Aim(); //Laser for Debuging Aim from gunBarrel Position
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            fireWeapon();
+        }
+    }
+
+    void fireWeapon()
+    {
+        gunBelt.ammoCount -= 1;
+        GameObject bullet = Instantiate(projectilePrefab, gunBarrel.transform.position, gunBarrel.transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * speed * Time.deltaTime);
+    }
+
+    //Debug Laser -- Click Gizmo to See in game in editer or to remove.
+    void Aim()
+    {
+        Vector3 screenCenter = transform.TransformDirection(Vector3.forward) * 100;
+        Debug.DrawRay(gunBarrel.transform.position, screenCenter, Color.green);
+    }
+}
+
