a8bbea00b9c5732359ddf96646689bb025d98ec3
arthura17 <arthura17@students.ecu.edu>
2021-05-02 23:32:39 -0400

    Victory menu

---
+++ Assets/Scripts/Victoryscreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victoryscreen : MonoBehaviour
+
+    
+{
+    public GameObject boss;
+
+    public GameObject victoryText;
+
+    public GameObject victoryButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(boss == null)
+        {
+            victoryButton.SetActive(true);
+            victoryText.SetActive(true);
+        }
+    }
+}
