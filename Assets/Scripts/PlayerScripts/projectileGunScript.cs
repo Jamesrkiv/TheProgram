34d7d9cbc55a1d53f7cb90421e1f2e73750e0ece
younglane <59858521+younglane@users.noreply.github.com>
2021-05-02 22:54:09 -0400

    gun sfx

--- Assets/Scripts/PlayerScripts/projectileGunScript.cs
+++ Assets/Scripts/PlayerScripts/projectileGunScript.cs
@@ -9,10 +9,16 @@ public class projectileGunScript : MonoBehaviour
     public bool debugLaser = false;
     private PlayerWeapons gunBelt;
 
+    //Audo
+    public AudioSource gunaudio;
+    public AudioClip gunsound;
+    public AudioClip gunswap;
+
     public float speed = 20;
     private void Start()
     {
         gunBelt = GetComponentInParent<PlayerWeapons>();
+        gunaudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,11 +29,16 @@ public class projectileGunScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             fireWeapon();
+            gunaudio.PlayOneShot(gunsound, .5f);
         }
     }
 
     void fireWeapon()
     {
+        if (gunBelt.ammoCount == 0)
+        {
+            gunaudio.PlayOneShot(gunswap, 1.0f);
+        }
         gunBelt.ammoCount -= 1;
         GameObject bullet = Instantiate(projectilePrefab, gunBarrel.transform.position, gunBarrel.transform.rotation);
         bullet.GetComponent<Rigidbody>().AddForce(transform.forward * speed * Time.deltaTime);
